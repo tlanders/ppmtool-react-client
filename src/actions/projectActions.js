e0cb1ec95d6ac09f19884872a8e10d3b579c2150
tlanders <768713+tlanders@users.noreply.github.com>
2019-12-03 20:28:26 -0600

    added code to post new project to spring api end of lecture 31

---
+++ src/actions/projectActions.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import {GET_ERRORS} from "./types";
+
+export const createProject = (project, history) => async dispatch => {
+    try {
+        const res = await axios.post(
+            'http://localhost:8080/api/project',
+            project
+        );
+
+        history.push("/dashboard"); // render project again
+    } catch(err) {
+        dispatch({
+            type : GET_ERRORS,
+            payload : err.response.data
+        });
+    }
+}
